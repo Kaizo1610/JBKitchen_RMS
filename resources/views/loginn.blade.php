170131d9a55af36880b4933ad0be64471bd580ee
myfzmri <fawwazmohamad1@gmail.com>
2024-01-09 21:07:31 +0800

    ok

--- resources/views/loginn.blade.php
+++ resources/views/loginn.blade.php
@@ -1,6 +1,13 @@
-@extends('master1.layout1')
-
-<section class="section" id="login">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <section class="section" id="login">
     <div>
         <h1>Choose your option</h1>
     </div>
@@ -10,13 +17,18 @@
         @auth
             <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
         @else
-            <li><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></li>
+            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
             @if (Route::has('register'))
-                <li><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></li>
+                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
             @endif
         @endauth
     </div>
 @endif
 
 </section>
+</body>
+</html>
+
+
+
