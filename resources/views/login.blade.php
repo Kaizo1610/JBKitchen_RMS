a8f09287d794aad67ed9d5b2148977148fed2641
myfzmri <fawwazmohamad1@gmail.com>
2024-01-09 20:18:06 +0800

    12

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,18 +1,14 @@
-<li>
-    @if (Route::has('login'))
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-@auth
-<a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-@else
-<li><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></li>
 
-@if (Route::has('register'))
-<li><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></li>
+@if (Route::has('login'))
+    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+        @else
+            <li><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></li>
+
+            @if (Route::has('register'))
+                <li><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></li>
+            @endif
+        @endauth
+    </div>
 @endif
-@endauth
-</div>
-@endif
-
-
-
-</li>
