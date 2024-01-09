62af8184350a317babd12e33b567b85436491888
myfzmri <fawwazmohamad1@gmail.com>
2024-01-09 18:03:57 +0800

    authen

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,13 @@ Route::get("/home", [HomeController::class,"index"]);
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
