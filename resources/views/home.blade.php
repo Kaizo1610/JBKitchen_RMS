58e0f49108b92f676c5822cbe116b485de3f7f3b
aimanakim <mohamadaiman896@gmail.com>
2024-01-14 13:33:08 +0800

    akim

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-lg-4">
                     <div class="left-content">
                         <div class="inner-content">
-                            <h4>JBKitchen</h4>
+                            <h4>KlassyCafe</h4>
                             <h6>THE BEST EXPERIENCE</h6>
                             <div class="main-white-button scroll-to-section">
                                 <a href="#reservation">Make A Reservation</a>
@@ -48,42 +48,42 @@
     </div>
     <!-- ***** Main Banner Area End ***** -->
 
-    <!-- ***** About Area Starts ***** -->
-    {{-- <section class="section" id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-6 col-xs-12">
-                    <div class="left-text-content">
-                        <div class="section-heading">
-                            <h6>About Us</h6>
-                            <h2>We Leave A Delicious Memory For You</h2>
-                        </div>
-                        <p>Klassy Cafe is one of the best <a href="https://templatemo.com/tag/restaurant" target="_blank" rel="sponsored">restaurant HTML templates</a> with Bootstrap v4.5.2 CSS framework. You can download and feel free to use this website template layout for your restaurant business. You are allowed to use this template for commercial purposes. <br><br>You are NOT allowed to redistribute the template ZIP file on any template donwnload website. Please contact us for more information.</p>
-                        <div class="row">
-                            <div class="col-4">
-                                <img src="assets/images/about-thumb-01.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img src="assets/images/about-thumb-02.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img src="assets/images/about-thumb-03.jpg" alt="">
-                            </div>
-                        </div>
+ <!-- ***** About Area Starts ***** -->
+ <section class="section" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                <div class="left-text-content">
+                    <div class="section-heading">
+                        <h6>About Us</h6>
+                        <h2>We Leave A Delicious Memory For You</h2>
                     </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-xs-12">
-                    <div class="right-content">
-                        <div class="thumb">
-                            <a rel="nofollow" href="http://youtube.com"><i class="fa fa-play"></i></a>
-                            <img src="assets/images/about-video-bg.jpg" alt="">
+                    <p>Klassy Cafe is one of the best in the world <a href="https://templatemo.com/tag/restaurant" target="_blank" rel="sponsored">restaurant HTML templates</a> with Bootstrap v4.5.2 CSS framework. You can download and feel free to use this website template layout for your restaurant business. You are allowed to use this template for commercial purposes. <br><br>You are NOT allowed to redistribute the template ZIP file on any template donwnload website. Please contact us for more information.</p>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="assets/images/about-thumb-01.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="assets/images/about-thumb-02.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="assets/images/about-thumb-03.jpg" alt="">
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                <div class="right-content">
+                    <div class="thumb">
+                        <a rel="nofollow" href="http://youtube.com"><i class="fa fa-play"></i></a>
+                        <img src="assets/images/about-video-bg.jpg" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
-    </section> --}}
-    <!-- ***** About Area Ends ***** -->
+    </div>
+</section>
+<!-- ***** About Area Ends ***** -->
 
     <!-- ***** Menu Area Starts ***** -->
     <section class="section" id="menu">
@@ -202,7 +202,7 @@
                             <img src="assets/images/chefs-01.jpg" alt="Chef #1">
                         </div>
                         <div class="down-content">
-                            <h4>Randy Orton</h4>
+                            <h4>Randy Walker</h4>
                             <span>Pastry Chef</span>
                         </div>
                     </div>
