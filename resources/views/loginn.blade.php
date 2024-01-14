d7913e52229f1dcd778873a3612f6e45d906ca62
myfzmri <fawwazmohamad1@gmail.com>
2024-01-14 11:15:02 +0800

    css

--- resources/views/loginn.blade.php
+++ resources/views/loginn.blade.php
@@ -6,6 +6,53 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<style>
+    body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+}
+
+.section {
+    text-align: center;
+    padding: 20px;
+    background-color: #fff;
+    margin: 50px auto;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    color: #333;
+}
+
+a {
+    text-decoration: none;
+    color: #333;
+    padding: 8px 16px;
+    border-radius: 4px;
+    transition: background-color 0.3s ease-in-out;
+}
+
+a:hover {
+    background-color: #ddd;
+}
+
+/* Responsive Styles */
+@media screen and (max-width: 768px) {
+    .section {
+        margin: 30px auto;
+        padding: 15px;
+    }
+
+    a {
+        display: block;
+        margin: 10px 0;
+    }
+}
+
+</style>
 <body>
     <section class="section" id="login">
     <div>
