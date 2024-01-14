4d48ff85627d5c0822c602cfadd09f686df3d6b8
Myrush <muhdarash73@gmail.com>
2024-01-14 12:57:08 +0800

    new

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\staffcontroller;
+use App\Http\Controllers\AdminController;
 
 
 /*
