2bff9d5112fef6f67a343bfde7e276d6177ebc70
myfzmri <fawwazmohamad1@gmail.com>
2024-01-16 00:35:38 +0800

    ii

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -4,11 +4,11 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                {{-- <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center">
                     <a href="/admin" class="logo">
                         <img src="assets/images/klassy-logo.png" align="klassy cafe html template">
                     </a>
-                </div> --}}
+                </div>
 
                 <!-- Navigation Links -->
                 {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
