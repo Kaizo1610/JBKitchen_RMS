5b3412d79c91d4cedfe27f81f50bd0935fd83ecd
B4DRI <badri.iium@gmail.com>
2024-01-17 15:47:33 +0800

    f

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,8 +74,7 @@
             <div class="col-lg-6 col-md-6 col-xs-12">
                 <div class="right-content">
                     <div class="thumb">
-                        <a rel="nofollow" href="http://youtube.com"><i class="fa fa-play"></i></a>
-                        <img src="assets/images/about-video-bg.jpg" alt="">
+                        <img src="assets/images/about-us.jpeg" alt="">
                     </div>
                 </div>
             </div>
