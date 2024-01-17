7056a2a5036ef3e7ff6c84919d7f0ec42d819439
aimanakim <mohamadaiman896@gmail.com>
2024-01-18 00:03:11 +0800

    akim

--- database/migrations/2024_01_17_154008_create_food_table.php
+++ database/migrations/2024_01_17_154008_create_food_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateFoodTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('food', function (Blueprint $table) {
             $table->id();
+            $table->string("title")->nullable();
+            $table->string("codeno")->nullable();
+            $table->string("image")->nullable();
+            $table->string("description")->nullable();
             $table->timestamps();
         });
     }
