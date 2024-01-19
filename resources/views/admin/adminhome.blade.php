be5082d2cdb61e86aebd1f1caf8c5a4e186ef1cc
aimanakim <mohamadaiman896@gmail.com>
2024-01-19 09:55:39 +0800

    akim

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -16,11 +16,12 @@
     @include("admin.navbar")
 
     <div style="position: relative; top:-650px; right: -350px ">
-        <h1>Welcome to Staff Homepage</h1>
-        <h2>Other staffs</h2>
+        <h1 style="color: lightcoral">Welcome to Staff Homepage</h1>
+        <br>
+        <h2 style="color: yellow">Achievement of our Staff</h2>
 
         <div class="row">
-        <div class="col-lg-4">
+        <div class="col-lg-2">
             <div class="chef-item">
                 <div class="thumb">
                     <div class="overlay"></div>
@@ -29,16 +30,16 @@
                         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                         <li><a href="#"><i class="fa fa-google"></i></a></li>
                     </ul>
-                    <img height="100px" width="300px" src="assets/images/fawwaz1.jpg" alt="Chef #3">
+                    <img height="50px" width="200px" src="assets/images/fawwaz1.jpg" alt="Chef #3">
                 </div>
                 <div class="down-content">
                     <h4>Fawwaz</h4>
-                    <span>Barista Chef</span>
+                    <span style="color: yellow">The Most Dedicated Chef</span>
                 </div>
             </div>
         </div>
 
-        <div class="col-lg-4">
+        <div class="col-lg-3">
             <div class="chef-item">
                 <div class="thumb">
                     <div class="overlay"></div>
@@ -47,11 +48,29 @@
                         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                         <li><a href="#"><i class="fa fa-google"></i></a></li>
                     </ul>
-                    <img height="100px" width="400px" src="assets/images/johan.jpg" alt="Chef #3">
+                    <img height="100px" width="300px" src="assets/images/johan.jpg" alt="Chef #3">
                 </div>
                 <div class="down-content">
                     <h4>Johan</h4>
-                    <span>Kitchen Helper</span>
+                    <span style="color: yellow">The Most Energetic Chef</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3">
+            <div class="chef-item">
+                <div class="thumb">
+                    <div class="overlay"></div>
+                    <ul class="social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google"></i></a></li>
+                    </ul>
+                    <img height="100px" width="300px" src="assets/images/arash.jpeg" alt="Chef #3">
+                </div>
+                <div class="down-content">
+                    <h4>Arash</h4>
+                    <span style="color: yellow">The Most Leadership Chef</span>
                 </div>
             </div>
         </div>
